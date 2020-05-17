134251b331b37c9e800cad3ef9cb738e70b68846
Mouna Belaid <belaid.mounaa@gmail.com>
2020-05-17 02:50:41 +0100

    Update refresh_YOURPACKAGENAME.R

--- R/refresh_YOURPACKAGENAME.R
+++ R/refresh_YOURPACKAGENAME.R
@@ -1,39 +1,19 @@
-#' SNAPPY TITLE ABOUT THIS FUNCTION
+#' Refresh data
 #'
-#' @description Updates the WHAT IS THIS DATASET
+#' @param verbose Should messages about what's going on be displayed?
 #'
-#' @details LONGER DESCRIPTION OF WHAT IS GOING ON HERE, THE DATA, IT'S LICENSE, ETC.
-#'
-#' @source WHERE DID YOU GET IT
-#' @source \href{https://github.com/Covid19R/documentation}{covid19R documentation}
-#'
-#' @return A tibble object
-#' * date - The date in YYYY-MM-DD form
-#' * location - The name of the location as provided by the data source. XXXXX
-#' * location_type - The type of location using the covid19R controlled vocabulary. SAY MORE ABOUT THIS
-#' * location_code - A standardized location code using a national or international standard. ADD MORE
-#' * location_code_type The type of standardized location code being used according to the covid19R controlled vocabulary. Here we use `XXXX`
-#' * data_type - the type of data in that given row using the covid19R controlled vocabulary. Includes WHAT DATA TYPES ARE HERE?
-#' * value - number of cases of each data type
-#' @export refresh_YOURPACKAGENAME
+#' @return A tibble of the most up to date per-day data on cases of COVID-19 in Tunisia, ordered by date descending.
+#' @export
 #'
 #' @examples
-#' \dontrun{
-#' # update the data
-#' YOURPACKAGENAME_dat <- refresh_YOURPACKAGENAME()
+#' \donttest{
+#' refresh_covid19france()
 #' }
-#'
-refresh_YOURPACKAGENAME <- function() {
-  
-  #where is the data coming from
-  url <- ""
+refresh_covid19tunisia <- function(verbose = TRUE) {
+  if (verbose) message(glue::glue("Downloading raw data from {url}."))
 
-  # load the data
-  
-  # clean the data if needed
-  
-  # reshape to data standard
-  
-  # return the data
+  read_data() %>%
+    clean_data() %>%
+    average_data() %>%
+    enlongen_data()
 }
-
