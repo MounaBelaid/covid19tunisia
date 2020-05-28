317cabe1115cecc3cf9c6b9c8ca636475d2e0233
Mouna Belaid <belaid.mounaa@gmail.com>
2020-05-28 19:44:16 +0100

    Update get_info.R

--- R/get_info.R
+++ R/get_info.R
@@ -7,34 +7,32 @@
 #' \donttest{
 #' get_info_covid19tunisia()
 #' }
-get_info_covid19tunisia <- function() {
- latest_data <-
-    refresh_covid19tunisia(verbose = FALSE)
-  
-  tibble::tribble(
-    ~data_set_name, ~package_name, ~function_to_get_data,
-    ~data_details, ~data_url, ~license_url,
-    ~data_types, ~location_types,
-    ~spatial_extent, ~has_geospatial_info,
 
-    "covid19tunisia",
-    "covid19tunisia",
-    "refresh_covid19tunisia",
-    "Open Source data on distribution of confirmed Covid-19 cases, recovered ones and deaths in Tunisia.
-For more, https://github.com/MounaBelaid/covid19datatunisia.",
-    "https://raw.githubusercontent.com/MounaBelaid/covid19datatunisia/master/dist/data.csv",
-    "https://github.com/MounaBelaid/covid19datatunisia/blob/master/LICENSE",
-     latest_data %>%
-       tidyr::drop_na(data_type) %>%
-       dplyr::pull(data_type) %>%
-       unique() %>%
-       stringr::str_c(collapse = ", "), #COMMA SEPARATED STRING OF DATA TYPES
-     latest_data %>%
-       tidyr::drop_na(location_type) %>%
-       dplyr::pull(location_type) %>%
-       unique() %>%
-       stringr::str_c(collapse = ", "), #COMMA SEPARATED STRING OF LOCATION TYPES
-    "country", #HOW LARGE IS THE AREA COVERED BY THE WHOLE DATASET? COUNTRY? CONTINENT? WORLD? OTHER?
-    FALSE #IS THERE GEOSPATIAL INFORMATION, E.G. LAT/LONG? TRUE/FALSE
+get_info_covid19tunisia <- function() {
+  latest_data <-
+    refresh_covid19tunisia(verbose = FALSE)
+
+  dplyr::tibble(
+    data_set_name = "covid19tunisia",
+    package_name = "covid19tunisia",
+    function_to_get_data = "refresh_covid19tunisia",
+    data_details = "Open Source data on distribution of confirmed Covid-19 cases, recovered ones and deaths in Tunisia.
+    For more, https://github.com/MounaBelaid/covid19datatunisia",
+    data_url = "https://raw.githubusercontent.com/MounaBelaid/covid19datatunisia/master/dist/data.csv",
+    license_url = "https://github.com/MounaBelaid/covid19datatunisia/blob/master/LICENSE",
+    data_types =
+      latest_data %>%
+        tidyr::drop_na(data_type) %>%
+        dplyr::pull(data_type) %>%
+        unique() %>%
+        stringr::str_c(collapse = ", "),
+    location_types =
+      latest_data %>%
+        tidyr::drop_na(location_type) %>%
+        dplyr::pull(location_type) %>%
+        unique() %>%
+        stringr::str_c(collapse = ", "),
+    spatial_extent = "country",
+    has_geospatial_info = FALSE
   )
 }
